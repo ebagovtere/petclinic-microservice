d72860a32758255c38fd426bbed144a59dac976f
ebagovtere <ebagovtere@gmail.com>
2023-01-28 11:24:28 +0000

    added qa automation pipeline for dev

--- selenium-jobs/test_owners_all_headless.py
+++ selenium-jobs/test_owners_all_headless.py
@@ -15,8 +15,8 @@ chrome_options.add_argument("disable-dev-shm-usage")
 driver = webdriver.Chrome(options=chrome_options)
 # driver = webdriver.Chrome("/Users/home/Desktop/chromedriver")
 # Connect to the application
-APP_IP = os.environ['MASTER_PUBLIC_IP']
-url = "http://"+APP_IP.strip()+":8080/"
+PP_IP = os.environ['MASTER_PUBLIC_IP']
+url = "http://"+APP_IP.strip()+":30001/"
 # url = "http://localhost:8080"
 print(url)
 driver.get(url)
